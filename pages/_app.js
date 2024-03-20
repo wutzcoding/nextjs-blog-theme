bf51c268da6b1298299b8c49db1207eb947f01de
wutzcoding <wutsuperwut@gmail.com>
2024-03-20 13:58:45 +0000

    tesT

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/globals.css';
-import 'prismjs/themes/prism-tomorrow.css';
+import '../styles/globals.css'; // Make sure you import your global styles
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <span className="theme-bejamas" />
-      <Component {...pageProps} />
-    </>
-  );
+  useEffect(() => {
+    // Add the 'dark' class to the HTML tag to enable dark mode by default
+    document.documentElement.classList.add('dark');
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
